4d4e1c6495ae36346fa034b4b344d784e93a839e
nagabala <nagabalaprime@gmail.com>
2022-04-24 01:38:31 +0530

    added certificate page

---
+++ src/components/CertificatePage/CertificateUpload/CertificateUpload.jsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import {
+    Footer
+} from "@microsoft/azureportal-reactview/Footer";
+
+
+/**
+ * Upload certificate and get base 64 encoding as the output.
+ */
+export const CertificateUpload = React.memo(
+  (props) => {
+    const [errors, setErrors] = React.useState([]);
+
+    return (
+      <Footer
+        
+      />
+    );
+  }
+);
